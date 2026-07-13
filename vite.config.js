e77b2665cd28638af65152e416584c7e85aa676b
luckys00 <luckys00@github.com>
2026-07-13 15:04:42 +0530

    chore: remove unused deps (axios, lucide-react), fix vite config, commit favicon

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // Disable Tailwind CSS auto-detection warning (we use vanilla CSS)
+    transformer: 'postcss',
+  },
 })
